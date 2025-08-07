ea8117a79d13b6103ba7cbc7a8a7b34286802a92
Fernando Silva <seuemail@email.com>
2025-08-06 23:59:01 -0300

    feat: estudos das aulas 26 a 31 da Maratona Java do DevDojo

---
+++ src/AcademyDev/Aula06EstruturasDeRepeticao05.java
@@ -0,0 +1,12 @@
+package AcademyDev;
+
+public class Aula06EstruturasDeRepeticao05 {
+	public static void main(String[] args) {
+		double valorTotal = 30000;
+		for (int parcela = (int) valorTotal; parcela >= 1; parcela--) {
+			double valorParcela = valorTotal / parcela;
+			System.out.println(" Parcela " + parcela + " R$ " + valorParcela);
+
+		}
+	}
+}
