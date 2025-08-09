7166b40875a8e64c08f9658a37c2106f4c3d4ecf
Fernando Silva <seuemail@email.com>
2025-08-09 19:39:44 -0300

    feat: estudo aulas 38 a 40 - arrays multidimensionais, criação e teste da classe Estudante

---
+++ src/AcademyDev/javacore/introducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,15 @@
+package AcademyDev.javacore.introducaoclasses.test;
+
+import AcademyDev.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+	public static void main(String[] args) {
+		Estudante estudante = new Estudante();
+		estudante.nome = "Fernando";
+		estudante.idade = 27;
+		estudante.sexo = 'M';
+		System.out.println(estudante.nome);
+		System.out.println(estudante.idade);
+		System.out.println(estudante.sexo);
+	}
+}
